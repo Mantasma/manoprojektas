8e0faf5d7df352bfaa568d42cc0d9485092c782b
mantasma <mant4s.mazeika@gmail.com>
2018-12-19 19:29:45 +0200

    Fix #2

--- app5.py
+++ app5.py
@@ -5,7 +5,7 @@ import sqlite3
 
 from sqlite3 import Error
 
-conn = sqlite3.connect('db16.sqlite')
+conn = sqlite3.connect('db17.sqlite')
 
 @app.route('/')
 def home():
@@ -18,7 +18,7 @@ def success(mastelis,nomenklatura,kiekis):
    irasas.write(mastelis)
    irasas.close()
 
-   c.execute("""INSERT INTO projects(mastelis, nomenklatura, kiekis) VALUES(?, ?, ?)""", (1,2,3))
+   conn.execute("""INSERT INTO projects(mastelis, nomenklatura, kiekis) VALUES(?, ?, ?)""", (1,2,3))
 
 
    return 'welcome %s' % mastelis
