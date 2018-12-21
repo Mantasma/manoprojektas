9f6acb74d67b73a370f4beff7a56dd59d4e623ce
mantasma <mant4s.mazeika@gmail.com>
2018-12-21 19:59:10 +0200

    Fix #5

--- maps_reguests_for_api.py
+++ maps_reguests_for_api.py
@@ -12,28 +12,13 @@ def home():
 
 @app.route('/maps/<mastelis>/<nomenklatura>/<kiekis>/')
 def success(mastelis,nomenklatura,kiekis):
-   # parsisiunciamas irasas txt formatu
-   irasas= open('atsakymas.txt','a')
-   irasas.write(mastelis)
-   irasas.close()
-   
    conn = sqlite3.connect('db1.sqlite')
-
    conn = conn.cursor()
+   conn.execute('''INSERT INTO projects(mastelis, nomenklatura,kiekis) VALUES(?, ?, ?)''', (mastelis, nomenklatura, kiekis))
 
-   conn.execute('''INSERT INTO projects(mastelis, nomenklatura,kiekis) VALUES(?, ?, ?)''', (10000, "N-5", 5))
-   conn.execute('''INSERT INTO projects(mastelis, nomenklatura,kiekis) VALUES(?, ?, ?)''', (20000, "N-7", 20))
-   conn.execute('''INSERT INTO projects(mastelis, nomenklatura,kiekis) VALUES(?, ?, ?)''', (25000, "N-14", 3))
-   conn.execute('''INSERT INTO projects(mastelis, nomenklatura,kiekis) VALUES(?, ?, ?)''', (100000, "N-21", 1))
-   conn.execute('''INSERT INTO projects(mastelis, nomenklatura,kiekis) VALUES(?, ?, ?)''', (250000, "N-15", 7))
+   conn.close()
 
-   conn.execute('''SELECT * FROM projects''')
-
-   data = conn.fetchall()
-
-   print (data)
-
-   return 'welcome %s' % mastelis
+   return 'sekmingai isirase'
 
 if __name__ == '__main__':
    app.run(port=5001,debug = True)
